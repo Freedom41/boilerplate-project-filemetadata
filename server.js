ff115a50b351ebe1c754c546cbe50f7911c3e274
Mark <s.markmas@gmail.com>
2019-09-09 22:12:06 +0530

    API

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
-
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
 // require and use "multer"...
-
-var app = express();
+const upload = multer()
+const port = 3000;
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -18,6 +19,16 @@ app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.post("/api/fileanalyse", upload.single('upfile'), (req,res,next) => {
+    next();
+    res.json({
+      "File-size": req.file.size,
+      "File-Extension": req.file.mimetype,
+      "File-name" : req.file.originalname
+    })
+
+});
+
+app.listen(port, function () {
   console.log('Node.js listening ...');
 });
